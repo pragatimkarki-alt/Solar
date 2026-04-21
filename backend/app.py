d09388b106278ec844b9e8673cf1a9c0bcabb035
pragatimkarki-alt <pragatimkarki@gmail.com>
2026-04-21 19:49:05 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -43,6 +43,5 @@ def predict():
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
